5d8baf26824f23385f6e53fd33f367d28011b0ea
Shane Howe <shanehowe100@hotmail.com>
2022-06-22 11:45:23 -0700

    Updates

---
+++ precache-manifest.e9b9dc7b2995b4abb07f747cb3c46974.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d2fb8a2ef1845e34505",
+    "url": "/ravenous/static/css/main.429c83f8.chunk.css"
+  },
+  {
+    "revision": "0d2fb8a2ef1845e34505",
+    "url": "/ravenous/static/js/main.0d2fb8a2.chunk.js"
+  },
+  {
+    "revision": "a87eee437a9272cc3145",
+    "url": "/ravenous/static/js/1.a87eee43.chunk.js"
+  },
+  {
+    "revision": "9f839d09ce85f2d9931f",
+    "url": "/ravenous/static/js/runtime~main.9f839d09.js"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "b77d891d866513de58b713632429c13c",
+    "url": "/ravenous/index.html"
+  }
+];
